d5654c60b38c910d55e16eaac8b54eb30c5a1d7f
Carl <carl@carlossus.com>
2015-07-18 14:41:19 +0800

    tests.common: added unit test for objectToArray

--- test/common.js
+++ test/common.js
@@ -1,10 +1,23 @@
 var max = require("../src/api/common/max");
+var objectToArray = require("../src/api/common/objectToArray");
 var chai = require("chai");
 var expect = chai.expect;
 describe("Common module unit tests", function () {
     it("will find the maximum number in a dataset", function () {
         var data = [1, 2, 3, 4, 5, 6, 7];
         expect(max(data)).to.equal(7);
+    });
+    it("will convert an object of string:number to an array", function () {
+        var obj = {
+            "one": 1,
+            "two": 2,
+            "three": 3
+        };
+        var data = objectToArray(obj);
+        expect(data.length).to.equal(3);
+        expect(data).to.contain(1);
+        expect(data).to.contain(2);
+        expect(data).to.contain(3);
     });
 });
 //# sourceMappingURL=common.js.map
--- test/common.ts
+++ test/common.ts
@@ -1,4 +1,5 @@
-import max = require("../src/api/common/max"); 
+import max = require("../src/api/common/max");
+import objectToArray = require("../src/api/common/objectToArray"); 
 import chai = require("chai");
 var expect = chai.expect;
 
@@ -7,6 +8,21 @@ describe("Common module unit tests", () => {
 	it("will find the maximum number in a dataset", () => {
 		var data = [1,2,3,4,5,6,7];		
 		expect(max(data)).to.equal(7);
+	});
+	
+	it("will convert an object of string:number to an array", () => {
+		var obj = {
+			"one": 1,
+			"two": 2,
+			"three": 3
+		};
+		
+		var data = objectToArray(obj);
+		
+		expect(data.length).to.equal(3);
+		expect(data).to.contain(1);
+		expect(data).to.contain(2);
+		expect(data).to.contain(3);
 	});
 
 });
