39c07b3428187fbabd8a7a8f9958e85c32b24b6d
Carl <carl@carlossus.com>
2015-07-18 17:13:46 +0800

    common.min: implemented module

---
+++ src/api/common/min.js
@@ -0,0 +1,20 @@
+var objectToArray = require("./objectToArray");
+/**
+ * @return Number Returns the highest value in a dataset/array
+ */
+function min(data) {
+    var dataset;
+    if (data instanceof Array)
+        dataset = data;
+    else
+        dataset = objectToArray(data);
+    var minimum = dataset.reduce(function (prev, curr) {
+        var isHigher = curr < prev;
+        if (isHigher)
+            prev = curr;
+        return prev;
+    }, 0);
+    return minimum;
+}
+module.exports = min;
+//# sourceMappingURL=min.js.map
---
+++ src/api/common/min.ts
@@ -0,0 +1,20 @@
+import objectToArray = require("./objectToArray");
+export = min;
+
+/**
+ * @return Number Returns the highest value in a dataset/array
+ */
+function min(data: number[]|{}) {
+	var dataset: number[];
+	if (data instanceof Array) dataset = data;
+	else dataset = objectToArray(data);
+	
+	var minimum = dataset.reduce((prev, curr) => {
+		let isHigher = curr < prev;
+		if (isHigher) prev = curr;
+		
+		return prev;
+	}, 0);
+	
+	return minimum;	
+}
