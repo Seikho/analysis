87a0ab9036520acd81e69ed1762de87c9e730c0a
Carl <carl@carlossus.com>
2015-07-21 19:27:35 +0800

    common.objectToArray: now throws when not passed an array or object

--- src/api/common/objectToArray.js
+++ src/api/common/objectToArray.js
@@ -1,6 +1,8 @@
 function convert(data) {
     if (data instanceof Array)
         return data.slice();
+    if (typeof data !== "object")
+        throw "TypeException: Must pass either an array or object of key-value pairs";
     var newData = Object.keys(data)
         .reduce(function (prev, curr) {
         var value = data[curr];
--- src/api/common/objectToArray.ts
+++ src/api/common/objectToArray.ts
@@ -1,7 +1,8 @@
 export = convert;
 
-function convert(data: number[]|{}) {
+function convert(data: number[]|{}) {	
 	if (data instanceof Array) return data.slice();
+	if (typeof data !== "object") throw "TypeException: Must pass either an array or object of key-value pairs";
 	
 	var newData = Object.keys(data)
 		.reduce((prev, curr) => {
