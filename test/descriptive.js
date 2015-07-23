4b651decf570eb459fa2def62c5b0fba2840b11c
Carl <carl@carlossus.com>
2015-07-23 21:33:07 +0800

    tests.descriptive: imports descriptive api instead of individual modules

--- test/descriptive.js
+++ test/descriptive.js
@@ -1,34 +1,28 @@
+var descriptive = require("../src/descriptive/api");
 var round = require("../src/common/round");
 var chai = require("chai");
 var expect = chai.expect;
-var mean = require("../src/descriptive/mean");
-var mode = require("../src/descriptive/mode");
-var median = require("../src/descriptive/median");
-var variance = require("../src/descriptive/variance");
-var stdDev = require("../src/descriptive/stdDev");
-var zScore = require("../src/descriptive/zScore");
-var firstQuartile = require("../src/descriptive/firstQuartile");
 describe("Descriptive module tests", function () {
     it("will find the mean for a dataset", function () {
         var data = [2, 3, 2, 3, 2, 3]; // 15	
-        expect(mean(data)).to.equal(2.5);
+        expect(descriptive.mean(data)).to.equal(2.5);
     });
     it("will find the mode for a unimodal dataset", function () {
         var data = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5];
-        var modes = mode(data);
+        var modes = descriptive.mode(data);
         expect(modes.length).to.equal(1);
         expect(modes[0]).to.equal(5);
     });
     it("will find the modes for a bimodal dataset", function () {
         var data = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6];
-        var modes = mode(data);
+        var modes = descriptive.mode(data);
         expect(modes.length).to.equal(2);
         expect(modes).to.contain(5);
         expect(modes).to.contain(6);
     });
     it("will find the modes for a multimodal (3+) dataset", function () {
         var data = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7];
-        var modes = mode(data);
+        var modes = descriptive.mode(data);
         expect(modes.length).to.equal(3);
         expect(modes).to.contain(5);
         expect(modes).to.contain(6);
@@ -36,32 +30,36 @@ describe("Descriptive module tests", function () {
     });
     it("will find the median for a dataset with 1 value", function () {
         var data = [5];
-        expect(median(data)).to.equal(5);
+        expect(descriptive.median(data)).to.equal(5);
     });
     it("will find the median for a dataset with 2 values", function () {
         var data = [5, 6];
-        expect(median(data)).to.equal(5.5);
+        expect(descriptive.median(data)).to.equal(5.5);
     });
     it("will find the median for a dataset with 3 values", function () {
         var data = [5, 10, 15];
-        expect(median(data)).to.equal(10);
+        expect(descriptive.median(data)).to.equal(10);
     });
     it("will calculate the variance of a population correctly", function () {
-        expect(variance([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).to.be.equal(8.25);
+        expect(descriptive.variance([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+            .to.be.equal(8.25);
     });
     it("will calculate the standard deviation", function () {
-        var theStdDev = round(stdDev([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), 2);
+        var theStdDev = round(descriptive.stdDev([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), 2);
         expect(theStdDev).to.equal(2.87);
     });
     it("will calculate the z-score correctly", function () {
-        expect(zScore([85, 90, 95, 100, 105, 110, 115], 110)).to.equal(1);
+        expect(descriptive.zScore([85, 90, 95, 100, 105, 110, 115], 110)).to.equal(1);
     });
     it("will calculate the first quartile", function () {
-        expect(firstQuartile([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).to.equal(3);
-        expect(firstQuartile([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])).to.equal(3);
+        expect(descriptive.firstQuartile([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+            .to.equal(3);
+        expect(descriptive.firstQuartile([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]))
+            .to.equal(3);
     });
     it("will throw if less than 4 values are supplied to first quartile", function () {
-        expect(firstQuartile.bind(firstQuartile, [1, 2, 3])).to.throw("Not enough values supplied");
+        expect(descriptive.firstQuartile.bind(descriptive.firstQuartile, [1, 2, 3]))
+            .to.throw("Not enough values supplied");
     });
 });
 //# sourceMappingURL=descriptive.js.map
--- test/descriptive.ts
+++ test/descriptive.ts
@@ -1,26 +1,19 @@
+import descriptive = require("../src/descriptive/api");
 import round = require("../src/common/round");
 import chai = require("chai");
 var expect = chai.expect;
-
-import mean = require("../src/descriptive/mean");
-import mode = require("../src/descriptive/mode");
-import median = require("../src/descriptive/median");
-import variance = require("../src/descriptive/variance");
-import stdDev = require("../src/descriptive/stdDev");
-import zScore = require("../src/descriptive/zScore");
-import firstQuartile = require("../src/descriptive/firstQuartile");
 
 describe("Descriptive module tests", () => {
 
 	it("will find the mean for a dataset", () => {
 		var data = [2, 3, 2, 3, 2, 3]; // 15	
-		expect(mean(data)).to.equal(2.5);
+		expect(descriptive.mean(data)).to.equal(2.5);
 	});
 
 	it("will find the mode for a unimodal dataset", () => {
 		var data = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5];
 
-		var modes = mode(data);
+		var modes = descriptive.mode(data);
 		expect(modes.length).to.equal(1);
 		expect(modes[0]).to.equal(5);
 	});
@@ -28,7 +21,7 @@ describe("Descriptive module tests", () => {
 	it("will find the modes for a bimodal dataset", () => {
 		var data = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6];
 
-		var modes = mode(data);
+		var modes = descriptive.mode(data);
 		expect(modes.length).to.equal(2);
 		expect(modes).to.contain(5);
 		expect(modes).to.contain(6);
@@ -37,7 +30,7 @@ describe("Descriptive module tests", () => {
 	it("will find the modes for a multimodal (3+) dataset", () => {
 		var data = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7];
 
-		var modes = mode(data);
+		var modes = descriptive.mode(data);
 		expect(modes.length).to.equal(3);
 		expect(modes).to.contain(5);
 		expect(modes).to.contain(6);
@@ -46,39 +39,43 @@ describe("Descriptive module tests", () => {
 
 	it("will find the median for a dataset with 1 value", () => {
 		var data = [5];
-		expect(median(data)).to.equal(5);
+		expect(descriptive.median(data)).to.equal(5);
 	});
 
 	it("will find the median for a dataset with 2 values", () => {
 		var data = [5, 6];
-		expect(median(data)).to.equal(5.5);
+		expect(descriptive.median(data)).to.equal(5.5);
 	});
 
 	it("will find the median for a dataset with 3 values", () => {
 		var data = [5, 10, 15];
-		expect(median(data)).to.equal(10);
+		expect(descriptive.median(data)).to.equal(10);
 	});
 
 	it("will calculate the variance of a population correctly", () => {
-		expect(variance([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).to.be.equal(8.25);
+		expect(descriptive.variance([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+			.to.be.equal(8.25);
 	});
 
 	it("will calculate the standard deviation", () => {
-		var theStdDev = round(stdDev([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), 2);
+		var theStdDev = round(descriptive.stdDev([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), 2);
 		expect(theStdDev).to.equal(2.87);
 	});
 
 	it("will calculate the z-score correctly", () => {
-		expect(zScore([85, 90, 95, 100, 105, 110, 115], 110)).to.equal(1);
+		expect(descriptive.zScore([85, 90, 95, 100, 105, 110, 115], 110)).to.equal(1);
 	});
 
 	it("will calculate the first quartile", () => {
-		expect(firstQuartile([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).to.equal(3);
-		expect(firstQuartile([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])).to.equal(3);
+		expect(descriptive.firstQuartile([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+			.to.equal(3);
+		expect(descriptive.firstQuartile([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]))
+			.to.equal(3);
 	});
-	
+
 	it("will throw if less than 4 values are supplied to first quartile", () => {
-		expect(firstQuartile.bind(firstQuartile, [1,2,3])).to.throw("Not enough values supplied");
+		expect(descriptive.firstQuartile.bind(descriptive.firstQuartile, [1, 2, 3]))
+			.to.throw("Not enough values supplied");
 	});
 });
 
