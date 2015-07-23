1a4e8333c4be8c55f48f12e135f339aec25ac630
Carl <carl@carlossus.com>
2015-07-23 21:40:15 +0800

    test.common: now uses common api module

--- test/common.js
+++ test/common.js
@@ -1,32 +1,26 @@
 var chai = require("chai");
 var expect = chai.expect;
-var max = require("../src/common/max");
-var min = require("../src/common/min");
-var range = require("../src/common/range");
-var sum = require("../src/common/sum");
-var objectToArray = require("../src/common/objectToArray");
-var round = require("../src/common/round");
-var curry = require("../src/common/curry");
+var common = require("../src/common/api");
 describe("Common module unit tests", function () {
     it("will curry a simple 2 argument function", function () {
         var add = function (left, right) { return left + right; };
-        var addOne = curry(add, 1);
+        var addOne = common.curry(add, 1);
         expect(addOne(2)).to.equal(3);
     });
     it("will throw when passed an invalid object", function () {
-        expect(objectToArray.bind(objectToArray, "bad type")).to.throw("Input must be array or object");
-        expect(objectToArray.bind(objectToArray, ["word", 1, 2, 3, 4])).to.throw("All values must be numbers");
+        expect(common.objectToArray.bind(common.objectToArray, "bad type")).to.throw("Input must be array or object");
+        expect(common.objectToArray.bind(common.objectToArray, ["word", 1, 2, 3, 4])).to.throw("All values must be numbers");
     });
     it("will find the maximum number in a dataset", function () {
-        expect(max([1, 2, 3, 4, 5, 6, 7])).to.equal(7);
-        expect(max([-1, -2, -3, -4, -5, -6, -7])).to.equal(-1);
+        expect(common.max([1, 2, 3, 4, 5, 6, 7])).to.equal(7);
+        expect(common.max([-1, -2, -3, -4, -5, -6, -7])).to.equal(-1);
     });
     it("will find the minimum number in a dataset", function () {
-        expect(min([1, 2, 3, 4, 5, 6, 7])).to.equal(1);
-        expect(min([-3, 0, 3])).to.equal(-3);
+        expect(common.min([1, 2, 3, 4, 5, 6, 7])).to.equal(1);
+        expect(common.min([-3, 0, 3])).to.equal(-3);
     });
     it("will find the range (min and max) in a dataset", function () {
-        var dataRange = range([10, 20, 30, 40, 50, 60]);
+        var dataRange = common.range([10, 20, 30, 40, 50, 60]);
         expect(dataRange.minimum).to.equal(10);
         expect(dataRange.maximum).to.equal(60);
     });
@@ -36,27 +30,27 @@ describe("Common module unit tests", function () {
             "two": 2,
             "three": 3
         };
-        var data = objectToArray(obj);
+        var data = common.objectToArray(obj);
         expect(data.length).to.equal(3);
         expect(data).to.contain(1);
         expect(data).to.contain(2);
         expect(data).to.contain(3);
     });
     it("will round a value correctly to 0 decimal places", function () {
-        var rounded = round(123.456);
+        var rounded = common.round(123.456);
         expect(rounded).to.equal(123);
     });
     it("will round a value correctly to 1 decimal places", function () {
-        var rounded = round(123.456, 1);
+        var rounded = common.round(123.456, 1);
         expect(rounded).to.equal(123.5);
     });
     it("will round a value correctly to 2 decimal places", function () {
-        var rounded = round(123.456, 2);
+        var rounded = common.round(123.456, 2);
         expect(rounded).to.equal(123.46);
     });
     it("will calculate the sum of a dataset", function () {
         var data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-        var total = sum(data);
+        var total = common.sum(data);
         expect(total).to.equal(55);
     });
 });
--- test/common.ts
+++ test/common.ts
@@ -1,39 +1,32 @@
 import chai = require("chai");
 var expect = chai.expect;
-
-import max = require("../src/common/max");
-import min = require("../src/common/min");
-import range = require("../src/common/range");
-import sum = require("../src/common/sum");
-import objectToArray = require("../src/common/objectToArray");
-import round = require("../src/common/round");
-import curry = require("../src/common/curry");
+import common = require("../src/common/api");
 
 describe("Common module unit tests", () => {
 
 	it("will curry a simple 2 argument function", () => {
 		var add = (left, right) => left + right;
-		var addOne = curry(add, 1);
+		var addOne = common.curry(add, 1);
 		expect(addOne(2)).to.equal(3);
 	});
 
 	it("will throw when passed an invalid object", () => {
-		expect(objectToArray.bind(objectToArray, "bad type")).to.throw("Input must be array or object");
-		expect(objectToArray.bind(objectToArray, ["word", 1, 2, 3, 4])).to.throw("All values must be numbers");
+		expect(common.objectToArray.bind(common.objectToArray, "bad type")).to.throw("Input must be array or object");
+		expect(common.objectToArray.bind(common.objectToArray, ["word", 1, 2, 3, 4])).to.throw("All values must be numbers");
 	});
 
 	it("will find the maximum number in a dataset", () => {
-		expect(max([1, 2, 3, 4, 5, 6, 7])).to.equal(7);
-		expect(max([-1, -2, -3, -4, -5, -6, -7])).to.equal(-1);
+		expect(common.max([1, 2, 3, 4, 5, 6, 7])).to.equal(7);
+		expect(common.max([-1, -2, -3, -4, -5, -6, -7])).to.equal(-1);
 	});
 
 	it("will find the minimum number in a dataset", () => {
-		expect(min([1, 2, 3, 4, 5, 6, 7])).to.equal(1);
-		expect(min([-3, 0, 3])).to.equal(-3);
+		expect(common.min([1, 2, 3, 4, 5, 6, 7])).to.equal(1);
+		expect(common.min([-3, 0, 3])).to.equal(-3);
 	});
 
 	it("will find the range (min and max) in a dataset", () => {
-		var dataRange = range([10, 20, 30, 40, 50, 60]);
+		var dataRange = common.range([10, 20, 30, 40, 50, 60]);
 		expect(dataRange.minimum).to.equal(10);
 		expect(dataRange.maximum).to.equal(60);
 	});
@@ -45,7 +38,7 @@ describe("Common module unit tests", () => {
 			"three": 3
 		};
 
-		var data = objectToArray(obj);
+		var data = common.objectToArray(obj);
 
 		expect(data.length).to.equal(3);
 		expect(data).to.contain(1);
@@ -54,23 +47,23 @@ describe("Common module unit tests", () => {
 	});
 
 	it("will round a value correctly to 0 decimal places", () => {
-		var rounded = round(123.456);
+		var rounded = common.round(123.456);
 		expect(rounded).to.equal(123);
 	});
 
 	it("will round a value correctly to 1 decimal places", () => {
-		var rounded = round(123.456, 1);
+		var rounded = common.round(123.456, 1);
 		expect(rounded).to.equal(123.5);
 	});
 
 	it("will round a value correctly to 2 decimal places", () => {
-		var rounded = round(123.456, 2);
+		var rounded = common.round(123.456, 2);
 		expect(rounded).to.equal(123.46);
 	});
 
 	it("will calculate the sum of a dataset", () => {
 		var data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-		var total = sum(data);
+		var total = common.sum(data);
 		expect(total).to.equal(55);
 	});
 });
