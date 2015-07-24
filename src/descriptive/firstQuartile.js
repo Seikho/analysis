c3995af10b578fb18f397d14233cb89be1dee2fd
Carl <carl@carlossus.com>
2015-07-24 18:11:18 +0800

    firstQuartile now uses common api

--- src/descriptive/firstQuartile.js
+++ src/descriptive/firstQuartile.js
@@ -1,12 +1,12 @@
-var objectToArray = require("../common/objectToArray");
+var common = require("../common/api");
 function firstQuartile(data) {
-    var dataset = objectToArray(data);
+    var dataset = common.toArray(data);
     if (dataset.length < 4)
         throw new Error("Not enough values supplied");
     var upperIndex = Math.floor(dataset.length / 2) - 1;
     var middleIndex = upperIndex / 2;
     dataset.sort(function (l, r) { return l - r; });
-    if (middleIndex % 2 === 0)
+    if (common.isEven(dataset.length))
         return dataset[middleIndex];
     var down = Math.floor(middleIndex);
     var up = Math.ceil(middleIndex);
--- src/descriptive/firstQuartile.ts
+++ src/descriptive/firstQuartile.ts
@@ -1,14 +1,16 @@
-import objectToArray = require("../common/objectToArray");
+import common = require("../common/api");
 export = firstQuartile;
 
 function firstQuartile(data: number[]|{}): number {
-	var dataset = objectToArray(data);
+	var dataset = common.toArray(data);
 	if (dataset.length < 4) throw new Error("Not enough values supplied");
+	
 	var upperIndex = Math.floor(dataset.length / 2) - 1;
 	var middleIndex = upperIndex / 2;
+	
 	dataset.sort((l, r) => l - r);
 
-	if (middleIndex % 2 === 0) return dataset[middleIndex];
+	if (common.isEven(dataset.length)) return dataset[middleIndex];
 
 	var down = Math.floor(middleIndex);
 	var up = Math.ceil(middleIndex);
