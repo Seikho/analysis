7c52b5b355bd10d40832b796a3f80e39a8ddeb10
Carl Winkler <carl.winkler86@gmail.com>
2015-07-24 23:40:46 +0800

    Added chi square exception unit tests

--- test/distribution.js
+++ test/distribution.js
@@ -1,10 +1,18 @@
 var chiSquare = require("../src/distribution/chiSquare");
 var chai = require("chai");
+var errors = require("../src/errors");
 var expect = chai.expect;
 describe("Distribution unit tests", function () {
     it("chiSquare: will correctly calculate the chi squared", function () {
         expect(chiSquare(10, 20)).to.equal(5);
         expect(chiSquare(20, 40)).to.equal(10);
+    });
+    it("chiSquare: will throw if either value is not a number", function () {
+        var fn = function (v1, v2) { return chiSquare.bind(chiSquare, v1, v2); };
+        expect(fn("string", 1)).to.throw(errors.AllMustBeNumbers);
+        expect(fn(1, "string")).to.throw(errors.AllMustBeNumbers);
+        expect(fn("string", "string")).to.throw(errors.AllMustBeNumbers);
+        expect(fn(NaN, NaN)).to.throw(errors.AllMustBeNumbers);
     });
 });
 //# sourceMappingURL=distribution.js.map
--- test/distribution.ts
+++ test/distribution.ts
@@ -1,6 +1,7 @@
 import chiSquare = require("../src/distribution/chiSquare");
 import round = require("../src/common/round");
 import chai = require("chai");
+import errors = require("../src/errors");
 var expect = chai.expect;
 
 describe("Distribution unit tests", () => {
@@ -8,6 +9,14 @@ describe("Distribution unit tests", () => {
 	it("chiSquare: will correctly calculate the chi squared", () => {
 		expect(chiSquare(10,20)).to.equal(5);
 		expect(chiSquare(20,40)).to.equal(10);
+	});
+	
+	it("chiSquare: will throw if either value is not a number", () => {
+		var fn = (v1, v2) => chiSquare.bind(chiSquare, v1, v2);
+		expect(fn("string", 1)).to.throw(errors.AllMustBeNumbers);
+		expect(fn(1, "string")).to.throw(errors.AllMustBeNumbers);
+		expect(fn("string", "string")).to.throw(errors.AllMustBeNumbers);
+		expect(fn(NaN, NaN)).to.throw(errors.AllMustBeNumbers);
 	});
 	
 });
