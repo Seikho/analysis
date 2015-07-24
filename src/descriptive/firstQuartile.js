c1bdff149ff5352f082548635748667920c15ddb
Carl <carl@carlossus.com>
2015-07-24 18:59:11 +0800

    firstQuartile: minor refactor, uses sortAsc module instead of sorting in the function

--- src/descriptive/firstQuartile.js
+++ src/descriptive/firstQuartile.js
@@ -1,13 +1,13 @@
 var toArray = require("../common/toArray");
 var isEven = require("../common/isEven");
+var sortAsc = require("../common/sortAsc");
 var errors = require("../errors");
 function firstQuartile(data) {
-    var dataset = toArray(data);
+    var dataset = sortAsc(toArray(data));
     if (dataset.length < 4)
         throw new Error(errors.InsufficientValues);
     var upperIndex = Math.floor(dataset.length / 2) - 1;
     var middleIndex = upperIndex / 2;
-    dataset.sort(function (l, r) { return l - r; });
     if (isEven(dataset.length))
         return dataset[middleIndex];
     var down = Math.floor(middleIndex);
--- src/descriptive/firstQuartile.ts
+++ src/descriptive/firstQuartile.ts
@@ -1,17 +1,16 @@
 import toArray = require("../common/toArray");
 import isEven = require("../common/isEven");
+import sortAsc = require("../common/sortAsc");
 import errors = require("../errors");
 export = firstQuartile;
 
 function firstQuartile(data: number[]|{}): number {
-	var dataset = toArray(data);
+	var dataset = sortAsc(toArray(data));
 	if (dataset.length < 4) throw new Error(errors.InsufficientValues);
 	
 	var upperIndex = Math.floor(dataset.length / 2) - 1;
 	var middleIndex = upperIndex / 2;
 	
-	dataset.sort((l, r) => l - r);
-
 	if (isEven(dataset.length)) return dataset[middleIndex];
 
 	var down = Math.floor(middleIndex);
