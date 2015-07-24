d8e4d9a4cd73e383a09292e306588053c1de9938
Carl <carl@carlossus.com>
2015-07-24 19:28:58 +0800

    Added frequency api module to main export

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 var common = require("./common/api");
 var descriptive = require("./descriptive/api");
+var frequency = require("./frequency/api");
 module.exports = {
     common: common,
     descriptive: descriptive,
-    frequency: null
+    frequency: frequency
 };
 //# sourceMappingURL=index.js.map
