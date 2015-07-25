bd5d7ef9dcc7b3e1c8fa3d517024f4b3355d74e7
Carl Winkler <carl.winkler86@gmail.com>
2015-07-25 18:33:48 +0800

    distribution tests now uses dist api. added poisson unit test

--- test/distribution.js
+++ test/distribution.js
@@ -1,18 +1,22 @@
-var chiSquare = require("../src/distribution/chiSquare");
+var dist = require("../src/distribution/api");
+var round = require("../src/common/round");
 var chai = require("chai");
 var errors = require("../src/errors");
 var expect = chai.expect;
 describe("Distribution unit tests", function () {
     it("chiSquare: will correctly calculate the chi squared", function () {
-        expect(chiSquare(10, 20)).to.equal(5);
-        expect(chiSquare(20, 40)).to.equal(10);
+        expect(dist.chiSquare(10, 20)).to.equal(5);
+        expect(dist.chiSquare(20, 40)).to.equal(10);
     });
     it("chiSquare: will throw if either value is not a number", function () {
-        var fn = function (v1, v2) { return chiSquare.bind(chiSquare, v1, v2); };
+        var fn = function (v1, v2) { return dist.chiSquare.bind(dist.chiSquare, v1, v2); };
         expect(fn("string", 1)).to.throw(errors.AllMustBeNumbers);
         expect(fn(1, "string")).to.throw(errors.AllMustBeNumbers);
         expect(fn("string", "string")).to.throw(errors.AllMustBeNumbers);
         expect(fn(NaN, NaN)).to.throw(errors.AllMustBeNumbers);
+    });
+    it("poisson: will correctly calculate the poisson distribution", function () {
+        expect(round(dist.poisson(3, 2), 2)).to.equal(0.18);
     });
 });
 //# sourceMappingURL=distribution.js.map
--- test/distribution.ts
+++ test/distribution.ts
@@ -1,4 +1,4 @@
-import chiSquare = require("../src/distribution/chiSquare");
+import dist = require("../src/distribution/api");
 import round = require("../src/common/round");
 import chai = require("chai");
 import errors = require("../src/errors");
@@ -7,16 +7,20 @@ var expect = chai.expect;
 describe("Distribution unit tests", () => {
 	
 	it("chiSquare: will correctly calculate the chi squared", () => {
-		expect(chiSquare(10,20)).to.equal(5);
-		expect(chiSquare(20,40)).to.equal(10);
+		expect(dist.chiSquare(10,20)).to.equal(5);
+		expect(dist.chiSquare(20,40)).to.equal(10);
 	});
 	
 	it("chiSquare: will throw if either value is not a number", () => {
-		var fn = (v1, v2) => chiSquare.bind(chiSquare, v1, v2);
+		var fn = (v1, v2) => dist.chiSquare.bind(dist.chiSquare, v1, v2);
 		expect(fn("string", 1)).to.throw(errors.AllMustBeNumbers);
 		expect(fn(1, "string")).to.throw(errors.AllMustBeNumbers);
 		expect(fn("string", "string")).to.throw(errors.AllMustBeNumbers);
 		expect(fn(NaN, NaN)).to.throw(errors.AllMustBeNumbers);
+	});
+	
+	it("poisson: will correctly calculate the poisson distribution", () => {
+		expect(round(dist.poisson(3,2),2)).to.equal(0.18);
 	});
 	
 });
