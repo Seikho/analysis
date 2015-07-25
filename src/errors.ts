4c17619d5410c7cfd2b04e734f526e0307887372
Carl Winkler <carl.winkler86@gmail.com>
2015-07-25 18:24:25 +0800

    added error messages for factorial

--- src/errors.ts
+++ src/errors.ts
@@ -5,5 +5,7 @@ var errors = {
 	AllMustBeNumbers: "All values must be numbers",
 	MustBeArrayOrObject: "Input must be array or object",
 	HistogramOneOption: "Must provide either binSize or binCount, but not both",
-	InsufficientValues: "Not enough values supplied"
+	InsufficientValues: "Not enough values supplied",
+	MustBeTwoOrMore: "Input must be at least 2",
+	MustBeWhole: "Input must be a whole number"
 }
