142c3873cc4599e481aa8a18d10985462455a2ec
Carl Winkler <carl.winkler86@gmail.com>
2015-07-25 18:08:46 +0800

    bugfix: tests werent running within helper module

--- test/helpers.js
+++ test/helpers.js
@@ -2,28 +2,18 @@ var chai = require("chai");
 var expect = chai.expect;
 function arrayIsEqual(left, right) {
     var isEqual = function (value, index) { return expect(value).to.equal(right[index]); };
-    describe("datasets are both Array", function () {
-        expect(Array.isArray(left)).to.equal(Array.isArray(right));
-    });
-    describe("datasets are the same length", function () {
-        expect(left.length).to.equal(right.length);
-    });
-    describe("dataset contents are equivalent", function () {
-        left.forEach(isEqual);
-    });
+    expect(Array.isArray(left)).to.equal(Array.isArray(right));
+    expect(left.length).to.equal(right.length);
+    left.forEach(isEqual);
 }
 exports.arrayIsEqual = arrayIsEqual;
 function datasetIsEquiv(left, right) {
     var rightHasKey = function (key) { return right[key] != null; };
-    var rightKeyIsSame = function (key) { return left[key] === right[key]; };
-    describe("datasets contain the same keys", function () {
-        var isTrue = Object.keys(left).every(rightHasKey);
-        expect(isTrue).to.be.true;
-    });
-    describe("datasets key-value pairs are equivalent", function () {
-        var isTrue = Object.keys(left).every(rightKeyIsSame);
-        expect(isTrue).to.be.true;
-    });
+    var isEqual = function (key) { return expect(left[key]).to.equal(right[key]); };
+    var isTrue = Object.keys(left).every(rightHasKey);
+    expect(isTrue).to.be.true;
+    Object.keys(left)
+        .forEach(isEqual);
 }
 exports.datasetIsEquiv = datasetIsEquiv;
 //# sourceMappingURL=helpers.js.map
--- test/helpers.ts
+++ test/helpers.ts
@@ -5,31 +5,21 @@ var expect = chai.expect;
 
 export function arrayIsEqual(left: any[], right: any[]) {
 	var isEqual = (value, index) => expect(value).to.equal(right[index]);
-	
-	describe("datasets are both Array", () => {
-		expect(Array.isArray(left)).to.equal(Array.isArray(right));
-	});
-	
-	describe("datasets are the same length", () => {
-		expect(left.length).to.equal(right.length);
-	});
 
-	describe("dataset contents are equivalent", () => {
-		left.forEach(isEqual);
-	});
+	expect(Array.isArray(left)).to.equal(Array.isArray(right));
+
+	expect(left.length).to.equal(right.length);
+
+	left.forEach(isEqual);
 }
 
 export function datasetIsEquiv(left: Analysis.Dataset, right: Analysis.Dataset) {
 	var rightHasKey = key => right[key] != null;
-	var rightKeyIsSame = key => left[key] === right[key];
+	var isEqual = key => expect(left[key]).to.equal(right[key]);
+	
+	var isTrue = Object.keys(left).every(rightHasKey);
+	expect(isTrue).to.be.true;
 
-	describe("datasets contain the same keys", () => {
-		var isTrue = Object.keys(left).every(rightHasKey);
-		expect(isTrue).to.be.true;
-	});
-
-	describe("datasets key-value pairs are equivalent", () => {
-		var isTrue = Object.keys(left).every(rightKeyIsSame);
-		expect(isTrue).to.be.true;
-	});
+	Object.keys(left)
+		.forEach(isEqual);
 }
