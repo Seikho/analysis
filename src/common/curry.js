f47eab7601d36c7dddd1c583c1fe627c84d2b30e
Carl Winkler <carl.winkler86@gmail.com>
2015-07-25 19:59:44 +0800

    curry: curry now accepts gaps (NaNs) and continues currying

--- src/common/curry.js
+++ src/common/curry.js
@@ -1,12 +1,42 @@
+function add(left, mid, right) {
+    return left + mid + right;
+}
+console.log(curry(add, NaN, NaN, 3)(1)(2));
 function curry(fn) {
     var fnArgs = [];
     for (var _i = 1; _i < arguments.length; _i++) {
         fnArgs[_i - 1] = arguments[_i];
     }
-    var args = Array.prototype.slice.call(arguments, 1);
+    while (fnArgs.length < fn.length) {
+        fnArgs[fnArgs.length] = NaN;
+    }
     return function () {
-        return fn.apply(this, args.concat(Array.prototype.slice.call(arguments, 0)));
+        var innerArgs = Array.prototype.slice.call(arguments, 0);
+        fnArgs = mergeArgs(fnArgs, innerArgs);
+        console.log(fnArgs);
+        if (fnArgs.some(isGap))
+            return curry.apply(curry, [fn].concat(fnArgs));
+        return fn.apply(this, fnArgs);
     };
+}
+function getFirstEmpty(args) {
+    for (var x = 0; x < args.length; x++) {
+        if (isGap(args[x]))
+            return x;
+    }
+    return null;
+}
+function mergeArgs(left, right) {
+    right.forEach(function (value) {
+        var next = getFirstEmpty(left);
+        if (next == null)
+            return;
+        left[next] = value;
+    });
+    return left;
+}
+function isGap(value) {
+    return isNaN(value) && value.toString() === "NaN";
 }
 module.exports = curry;
 //# sourceMappingURL=curry.js.map
--- src/common/curry.ts
+++ src/common/curry.ts
@@ -1,9 +1,47 @@
 export = curry;
 
+function add(left, mid, right) {
+	return left + mid + right;
+}
+
+console.log(curry(add, NaN, NaN, 3)(1)(2));
+
 function curry(fn: Function, ...fnArgs: any[]): (...args: any[]) => any {
-	var args = Array.prototype.slice.call(arguments, 1);
+	while(fnArgs.length < fn.length) {
+		fnArgs[fnArgs.length] = NaN;
+	}
 	
 	return function() {
-		return fn.apply(this, args.concat(Array.prototype.slice.call(arguments, 0)));
+		let innerArgs = Array.prototype.slice.call(arguments, 0);
+		
+		fnArgs = mergeArgs(fnArgs, innerArgs);
+		console.log(fnArgs);
+		if (fnArgs.some(isGap))
+			return curry.apply(curry, [fn].concat(fnArgs));
+			
+		return fn.apply(this, fnArgs);
 	};
+}
+
+function getFirstEmpty(args: any[]) {
+	for (let x = 0; x < args.length; x++) {
+		if (isGap(args[x])) return x;
+	}
+	
+	return null;
+}
+
+function mergeArgs(left: any[], right: any[]) {
+
+	right.forEach(value => {
+		let next = getFirstEmpty(left);
+		if (next == null) return;
+		left[next] = value;
+	});
+	
+	return left;
+}
+
+function isGap(value: any) {
+	return isNaN(value) && value.toString() === "NaN";
 }
