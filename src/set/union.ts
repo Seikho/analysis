a2bb6907c926923aec84bb7db6e36f2d8bf5be94
Carl <carl@carlossus.com>
2015-07-26 19:41:04 +0800

    implemented union

---
+++ src/set/union.ts
@@ -0,0 +1,9 @@
+import distinct = require("./distinct");
+export = union;
+
+function union(left: number[]|{}, right: number[]|{}): number[] {
+	var allValues = distinct(left)
+		.concat(distinct(right));
+
+	return distinct(allValues);
+}
