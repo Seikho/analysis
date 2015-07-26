55f8b82528159b0a45b825a283dd3a2612a05530
Carl <carl@carlossus.com>
2015-07-26 18:52:24 +0800

    naively added browserify to build script

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 var common = require("./common/api");
 var descriptive = require("./descriptive/api");
 var frequency = require("./frequency/api");
+var distribution = require("./distribution/api");
 module.exports = {
     common: common,
     descriptive: descriptive,
-    frequency: frequency
+    frequency: frequency,
+    distribution: distribution
 };
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import common = require("./common/api");
 import descriptive = require("./descriptive/api");
 import frequency = require("./frequency/api");
+import distribution = require("./distribution/api");
 
 export = {
 	common: common,
 	descriptive: descriptive,
-	frequency: frequency
+	frequency: frequency,
+	distribution: distribution	
 };
