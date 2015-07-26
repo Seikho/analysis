850760bdea087928023619f64c60be68cfe4a27b
Carl Winkler <carl.winkler86@gmail.com>
2015-07-26 13:55:56 +0800

    updated error message for clarity

--- src/errors.ts
+++ src/errors.ts
@@ -5,7 +5,7 @@ var errors = {
 	AllMustBeNumbers: "All values must be numbers",
 	MustBeArrayOrObject: "Input must be array or object",
 	HistogramOneOption: "Must provide either binSize or binCount, but not both",
-	InsufficientValues: "Not enough values supplied",
+	InsufficientValues: "Not enough inputs supplied",
 	MustBeAtLeastOne: "Input must at least 1",
 	MustBeWhole: "Input must be a whole number",
 	AllMustBeFunctions: "All inputs must be functions",
