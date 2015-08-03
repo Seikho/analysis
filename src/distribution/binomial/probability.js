47f17f4499736a2c9a61639223ef634f2c250ac7
Carl <carl@carlossus.com>
2015-08-03 22:14:44 +0800

    Implement binomial probability: generalised k successes in n attempts

--- src/distribution/binomial/probability.js
+++ src/distribution/binomial/probability.js
@@ -1,5 +1,9 @@
-function probability(changeOfSuccess, events, expectedSuccesses) {
-    return 0;
+var coefficient = require("./coefficient");
+function probability(chanceOfSuccess, events, expectedSuccesses) {
+    var odds = coefficient(events, expectedSuccesses);
+    var success = Math.pow(chanceOfSuccess, expectedSuccesses);
+    var fail = Math.pow(1 - chanceOfSuccess, events - expectedSuccesses);
+    return odds * success * fail;
 }
 module.exports = probability;
 //# sourceMappingURL=probability.js.map
--- src/distribution/binomial/probability.ts
+++ src/distribution/binomial/probability.ts
@@ -1,6 +1,10 @@
 import coefficient = require("./coefficient");
 export = probability;
 
-function probability(changeOfSuccess: number, events: number, expectedSuccesses: number): number {
-	return 0;
+function probability(chanceOfSuccess: number, events: number, expectedSuccesses: number): number {
+	var odds = coefficient(events, expectedSuccesses);
+	var success = Math.pow(chanceOfSuccess, expectedSuccesses);
+	var fail = Math.pow(1 - chanceOfSuccess, events - expectedSuccesses);
+	
+	return odds * success * fail;
 }
