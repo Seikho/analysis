fbf4b378a6d8dd83cabe4c80d2df14f71b4c3a1f
Carl Winkler <carl@carlossus.com>
2015-08-17 12:05:40 +0800

    Fix sum bug that would not reduce an empty array to 0

--- analysis.js
+++ analysis.js
@@ -236,7 +236,7 @@ var toArray = require("./toArray");
 function sum(data) {
     var dataset = toArray(data);
     var add = function (left, right) { return left + right; };
-    return dataset.reduce(add);
+    return dataset.reduce(add, 0);
 }
 module.exports = sum;
 
